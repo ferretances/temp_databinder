0d7265d13bc11d0be3d4701ef20457382c8d9bda
eferreira <eferreira@kitsd.com>
2011-10-20 16:15:01 -0400

    fbencosme working

--- databinder-app-hib/src/main/java/net/databinder/hib/conv/components/IConversationPage.java
+++
@@ -1,8 +0,0 @@
-package net.databinder.hib.conv.components;
-
-import org.hibernate.classic.Session;
-
-public interface IConversationPage {
-	public Session getConversationSession(Object key);
-	public void setConversationSession(Object key, Session conversationSession);
-}
